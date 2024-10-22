d273f83341bbc55bc6373cc4708ae0f9e5a64c1f
gazziiii777 <you@example.com>
2024-10-22 23:02:48 +0300

    а вот с докером у меня какие-о проблемы, он не видит файл database

--- app/main.py
+++ app/main.py
@@ -1,18 +1,16 @@
 from fastapi import FastAPI
-import database
 from app.api_v1.products import router as products_router
 from app.api_v1.orders import router as orders_router
+import database
 
 database.Base.metadata.drop_all(bind=database.engine)
 database.Base.metadata.create_all(bind=database.engine)
-
 
 app = FastAPI()
 
 app.include_router(products_router, prefix="/products", tags=["products"])
 app.include_router(orders_router, prefix="/orders", tags=["orders"])
 
-# Запуск сервера
 if __name__ == "__main__":
     import uvicorn
 
